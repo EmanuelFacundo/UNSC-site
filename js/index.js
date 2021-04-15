6bb21f74a0d201f0c4b638e3b0abbbbf4d0a6ef0
Emanuel <emanuel.facundo14@gmail.com>
2021-04-15 19:45:04 -0300

    Card

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 function hambuguer(){
-    const element = document.getElementById("menu")
+    const element = document.getElementById("myMenu")
 
     if(element.classList){
         element.classList.toggle("active")
     } else {
         const classes = element.className.split(" ")
-        const i = classes.indexOf("menu")
+        const i = classes.indexOf("myMenu")
 
         if(i >= 0) {
             classes.splice(i, 1)
         } else {
-            classes.push("menu")
+            classes.push("myMenu")
             element.className = classes.join(" ")
         }
 
